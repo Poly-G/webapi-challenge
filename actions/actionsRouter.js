c29e085df6021dbcfbf6601b6c623d500fbb6af6
Poly Goveia <poly.goveia@gmail.com>
2019-08-03 12:51:38 -0700

    add get actions

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,7 +1,20 @@
-// const express = require("express");
+const express = require("express");
+const db = require("../data/helpers/actionModel");
+const dbProjects = require("../data/helpers/projectModel");
+const router = express.Router();
 
-// const db = require("../data/helpers/projectModel");
+// send a GET (READ) request to retrieve all actions
+router.get("/", async (req, res) => {
+  try {
+    const actions = await db.get(res.query);
+    if (actions) {
+      res.json(actions);
+    } else {
+      res.status(400).json({ message: "actions not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-// const router = express.Router();
-
-// module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 
 const projectsRouter = require("./projects/projectsRouter");
-// const actionsRouter = require("./actions/actionsRouter");
+const actionsRouter = require("./actions/actionsRouter");
 
 const server = express();
 server.use(express.json());
@@ -12,7 +12,7 @@ server.use(helmet());
 server.use(morgan("dev"));
 
 server.use("/api/projects", projectsRouter);
-// server.use("/api/actions", actionsRouter);
+server.use("/api/actions", actionsRouter);
 
 server.get("/", (req, res) => {
   res.send("Works");
