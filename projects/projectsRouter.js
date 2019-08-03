a2a6ed865428d46b5361977ffbf7570611511535
Poly Goveia <poly.goveia@gmail.com>
2019-08-03 11:10:41 -0700

    add post to projects

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -7,6 +7,8 @@ const router = express.Router();
 //U-PDATE
 //D-ELETE
 
+// get - post - put - delete
+
 // send a GET (READ) request to retrieve all projects
 router.get("/", async (req, res) => {
   try {
@@ -22,13 +24,25 @@ router.get("/", async (req, res) => {
 });
 
 // send a GET (READ) request to retrieve a single project
-
 router.get("/:id", validateID, async (req, res) => {
   const project = await db.get(req.params.id);
   res.json(project);
 });
 
-//controller
+// send a POST (CREATE) request to create a project
+router.post("/", async (req, res) => {
+  try {
+    const project = await db.insert(req.body);
+    res.json(project);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// send a PUT (UPDATE) request to edit a project
+
+// send a DELETE request to delete a project
+
 async function validateID(req, res, next) {
   const { id } = req.params;
   if (!id) {
