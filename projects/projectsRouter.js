6d23fd836486df61d0c8050ac88f79f95c0eff2a
Poly Goveia <poly.goveia@gmail.com>
2019-08-03 10:44:16 -0700

    add get to projects

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -2,4 +2,18 @@ const express = require("express");
 const db = require("../data/helpers/projectModel");
 const router = express.Router();
 
+// send a GET request to retrieve all projects
+router.get("/", async (req, res) => {
+  try {
+    const projects = await db.get(res.query);
+    if (projects) {
+      res.json(projects);
+    } else {
+      res.status(404).json({ message: "projects not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const projectsRouter = require("./projects/projectsRouter");
+// const actionsRouter = require("./actions/actionsRouter");
+
 const server = express();
 server.use(express.json());
-
-const projectsRouter = require("./projects/projectsRouter");
-const actionsRouter = require("./actions/actionsRouter");
 
 server.use(helmet());
 server.use(morgan("dev"));
 
 server.use("/api/projects", projectsRouter);
-server.use("/api/actions", actionsRouter);
+// server.use("/api/actions", actionsRouter);
 
 server.get("/", (req, res) => {
   res.send("Works");
