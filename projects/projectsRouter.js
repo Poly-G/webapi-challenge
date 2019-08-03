8c699f3be95be3f2e6c67c766895729a9c8645ce
Poly Goveia <poly.goveia@gmail.com>
2019-08-03 11:03:06 -0700

    add middleware and a single get request

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -2,7 +2,12 @@ const express = require("express");
 const db = require("../data/helpers/projectModel");
 const router = express.Router();
 
-// send a GET request to retrieve all projects
+//C-REATE
+//R-EAD
+//U-PDATE
+//D-ELETE
+
+// send a GET (READ) request to retrieve all projects
 router.get("/", async (req, res) => {
   try {
     const projects = await db.get(res.query);
@@ -15,5 +20,27 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// send a GET (READ) request to retrieve a single project
+
+router.get("/:id", validateID, async (req, res) => {
+  const project = await db.get(req.params.id);
+  res.json(project);
+});
+
+//controller
+async function validateID(req, res, next) {
+  const { id } = req.params;
+  if (!id) {
+    res.status(400).json({ message: "Missing ID" });
+  } else {
+    const idValidated = await db.get(id);
+    if (!idValidated) {
+      res.status(404).json({ message: "ID not found" });
+      return;
+    }
+  }
+  next();
+}
 
 module.exports = router;
